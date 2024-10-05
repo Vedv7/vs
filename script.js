e3c8ec86bfc459bc1e4d43852dc76e35631fed66
VedaSwaroop  Dasagrandhi <161063269+Vedv7@users.noreply.github.com>
2024-10-05 18:06:45 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleCard(card) {
-    var content = card.querySelector('.card-content');
-    if (content.style.display === 'block') {
-        content.style.display = 'none';
-    } else {
-        content.style.display = 'block';
-    }
-}
